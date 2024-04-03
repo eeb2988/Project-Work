47bfc35232a5a452933af3d0d20f28158a11a990
Ethan Borys <eeb2988@rit.edu>
2024-04-03 11:16:07 -0400

    Model Trained Loss file created

---
+++ TestTrain.py
@@ -0,0 +1,97 @@
+from DenseNet import DenseNet
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.optim as optim
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+def main():
+
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    
+
+    
+    # get some random training images
+    dataiter = iter(trainloader)
+    images, labels = next(dataiter)
+
+    # show images
+    imshow(torchvision.utils.make_grid(images))
+    # print labels
+    print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+
+    dn = DenseNet(growth_rate=32, num_init_features=64, bn_size=4, drop_rate=0, num_classes=10, memory_efficient=False)
+
+    model_on_gpu = dn.to(device)
+
+
+
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model_on_gpu.parameters(), lr=0.001, momentum=0.9)
+
+
+
+
+    for epoch in range(50):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model_on_gpu(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+            
+    torch.save(dn.state_dict(), 'savedmodel.pt')
+
+        #Later to restore:
+        
+
+    print('Finished Training')
+
+    
+if __name__ == '__main__':
+    main()
