c91bc07c844a2e1d4a70040cef59186b78b2fc78
Ethan Borys <eeb2988@rit.edu>
2024-04-05 08:20:08 -0400

    Trained the new model

--- TestTrain.py
+++ TestTrain.py
@@ -5,6 +5,8 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
 import torch.optim as optim
+import csv
+import pandas as pd
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -45,10 +47,10 @@ def main():
     dataiter = iter(trainloader)
     images, labels = next(dataiter)
 
-    # show images
-    imshow(torchvision.utils.make_grid(images))
-    # print labels
-    print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+    # # show images
+    # imshow(torchvision.utils.make_grid(images))
+    # # print labels
+    # print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
 
     dn = DenseNet(growth_rate=32, num_init_features=64, bn_size=4, drop_rate=0, num_classes=10, memory_efficient=False)
 
@@ -59,16 +61,22 @@ def main():
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = optim.SGD(model_on_gpu.parameters(), lr=0.001, momentum=0.9)
 
-
-
+    accuracy_matrix = np.matrix([['epoch','accuracy']])
+    loss_matrix = np.matrix([['epoch', 'loss']])
+    # accuracy_matrix = np.append(accuracy_matrix,np.array([[1,2]]), axis=0)
 
     for epoch in range(50):  # loop over the dataset multiple times
 
+        correct = 0
+        total_for_acc = 0
         running_loss = 0.0
+        total_loss = 0.0
+        total_count = 0
         for i, data in enumerate(trainloader, 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
-
+            inputs = inputs.to(device)
+            labels = labels.to(device)
             # zero the parameter gradients
             optimizer.zero_grad()
 
@@ -82,10 +90,37 @@ def main():
             running_loss += loss.item()
             if i % 2000 == 1999:    # print every 2000 mini-batches
                 print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                print(running_loss/2000)
+                total_loss += running_loss
+                total_count += 1
                 running_loss = 0.0
 
-            
-    torch.save(dn.state_dict(), 'savedmodel.pt')
+        loss_matrix = np.append(loss_matrix, np.array([[epoch,total_loss/(2000*total_count)]]), axis=0)
+        total_count = 0
+        total_loss = 0.0    
+
+        for i, data in enumerate(testloader, 0):
+            inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model_on_gpu(inputs)
+            _, predicted = torch.max(outputs,1)
+
+            correct += (predicted == labels).sum().item()
+            total_for_acc += labels.size(0)
+        accuracy_matrix = np.append(accuracy_matrix, np.array([[epoch, correct / total_for_acc]]),axis=0)
+        print(correct/total_for_acc)
+
+        correct = 0
+        incorrect = 0 
+
+    DF = pd.DataFrame(loss_matrix) 
+    DF.to_csv("Metric_Loss.csv", sep=',', header=False, index=False)
+
+    DF2 = pd.DataFrame(accuracy_matrix)
+    DF2.to_csv("Metric_Accuracy.csv", sep=',', header=False, index=False)
+    torch.save(dn.state_dict(), 'Finished_Model.pt')
 
         #Later to restore:
         
